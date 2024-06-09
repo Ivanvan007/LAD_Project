976877258fea00e4ebf60e2a5d2c9b7402fc9b1e
Ivanvan007 <ivan.ibanhez.vilar@gmail.com>
2024-06-09 17:55:32 +0100

    V0.6.4

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ full_path = os.path.join(file_path, file_name)
 #Start CSV File Reading
 #Ler-se-ão apenas as primeiras 180000 linhas do arquivo
 dfGPlayStore = pd.read_csv(full_path,nrows=500000)
-#dfGPlayStore = pd.read_csv(full_path) #dfGPlayStore = dfGPlayStore.dropna()
+#dfGPlayStore = pd.read_csv(full_path) #descomentar para usar o dataset enteiro. comentar a linha de cima #dfGPlayStore = dfGPlayStore.dropna()
 #CSV File Reading Finished
 
 
